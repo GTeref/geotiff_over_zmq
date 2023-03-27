b1f296afd6453b6434e89915428250db67d19df6
Giovanni Terefi <giovanni.terefi@stonybrook.edu>
2023-03-26 23:26:31 -0400

    renamed files

--- sub.py
+++ sub.py
@@ -7,10 +7,10 @@ context = zmq.Context()
 socket = context.socket(zmq.SUB)
 socket.connect("tcp://0.0.0.0:135")
 socket.setsockopt(zmq.SUBSCRIBE, '')
-path = 'C:\Personal Projects\geotiff_over_zmq\sample_folder'
+path = os.path.join("test_folder")
 
 while True:
     message = socket.recv()
-    print("Received request: %s" % message)
+    print("Received file: %s message")
     time.sleep(1)
-    socket.send(b"World")
+    socket.send(b"File received!")
