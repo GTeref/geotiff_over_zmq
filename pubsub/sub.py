34633d8de8dcce8b5342937dd3e394ef2aefc621
Giovanni Terefi <giovanni.terefi@stonybrook.edu>
2023-03-28 23:22:55 -0400

    pub.py now works; sub.py still needs work

--- pubsub/sub.py
+++ pubsub/sub.py
@@ -4,23 +4,28 @@ import tqdm
 import socket
 import os
 
+host = "0.0.0.0"
+port = 5001
 BUFFER_SIZE = 4096
+separator = "<SEPARATOR>"
 
-while True:
 
-    print("Connecting to GEOTIFF over ZMQ...")
-    context = zmq.Context()
-    subscriber = context.socket(zmq.SUB)
-    subscriber.connect("tcp://*:5555")
-    subscriber.setsockopt_string(zmq.SUBSCRIBE, '')
+print("Connecting to GEOTIFF over ZMQ...")
+context = zmq.Context()
+subscriber = context.socket(zmq.SUB)
+subscriber.bind("tcp://*:5555")
+subscriber.setsockopt_string(zmq.SUBSCRIBE, '')
 
-    fileBuffer = subscriber.recv(copy=False).buffer()
-    subscriber.send("Received!")
-    fileName = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'sample.tif')
-    fileHandle = open(fileName, "w")
-    fileHandle.write(fileBuffer)
-    fileHandle.close
-    subscriber.close
-    context.term
+print(f"Listening over {subscriber}...")
+
+
+fileBuffer = subscriber.recv.buffer()
+subscriber.send("Received!")
+fileName = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'sample.tif')
+fileHandle = open(fileName, "w")
+fileHandle.write(fileBuffer)
+fileHandle.close
+subscriber.close
+context.term
     
-    time.sleep(5)
+time.sleep(5)
