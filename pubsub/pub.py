516017b5fb1f307a4f75d5956832fb91551b514a
Giovanni Terefi <giovanni.terefi@stonybrook.edu>
2023-03-29 17:27:06 -0400

    performed some wizardry to make this work using ZMQ's pub/sub model

--- pubsub/pub.py
+++ pubsub/pub.py
@@ -6,19 +6,20 @@ import os
 
 separator = "<SEPARATOR>"
 BUFFER_SIZE = 4096
+serverAddress = "tcp://localhost:5555"
 fileName = "sample.tif"
 
 print("GEOTIFF over ZMQ initializing...")
 context = zmq.Context()
 print("Creating publisher socket....")
 publisher = context.socket(zmq.PUB)
-publisher.connect("tcp://localhost:5555")
-print(f"Connecting to {publisher}...")
+publisher.connect(serverAddress)
+print(f"Connecting to {serverAddress}...")
 time.sleep(1)
 size = os.path.getsize(fileName)
 print(f"The size of {fileName} is: {size} bytes.")
 
-publisher.send_string(f"{fileName}{separator}{size}")
+publisher.send(f"{fileName}{separator}{size}".encode())
 
 progress = tqdm.tqdm(range(size), f"Sending {fileName}", unit="B", unit_scale=True, unit_divisor=1024)
 with open(fileName, "rb") as f:
--- pubsub/sub.py
+++ pubsub/sub.py
@@ -4,28 +4,38 @@ import tqdm
 import socket
 import os
 
-host = "0.0.0.0"
-port = 5001
 BUFFER_SIZE = 4096
 separator = "<SEPARATOR>"
-
+clientAddress = "tcp://*:5555"
 
 print("Connecting to GEOTIFF over ZMQ...")
 context = zmq.Context()
 subscriber = context.socket(zmq.SUB)
-subscriber.bind("tcp://*:5555")
+subscriber.bind(clientAddress)
 subscriber.setsockopt_string(zmq.SUBSCRIBE, '')
 
 print(f"Listening over {subscriber}...")
+fileSocket, serverAddress = subscriber.recv_string()
+fileSocket = context.socket(zmq.SUB)
+print(f"{serverAddress} has connected to {clientAddress}.")
 
+fileBuffer = fileSocket.recv(BUFFER_SIZE).decode()
+fileName, size = fileBuffer.split(separator)
+fileName = os.path.basename(fileName)
+size = int(size)
 
-fileBuffer = subscriber.recv.buffer()
-subscriber.send("Received!")
-fileName = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'sample.tif')
-fileHandle = open(fileName, "w")
-fileHandle.write(fileBuffer)
-fileHandle.close
+progress = tqdm.tqdm(range(size), f"Receiving {fileName}", unit="B", unit_scale=True, unit_divisor=1024)
+
+with open(fileName, "rb") as f:
+    while True:
+        bytes_read = fileSocket.recv(BUFFER_SIZE)
+        if not bytes_read:
+            break
+        else:
+            f.write(bytes_read)
+            progress.update(len(bytes_read))
+
+fileSocket.close
 subscriber.close
-context.term
     
 time.sleep(5)
