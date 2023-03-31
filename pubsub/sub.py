b99585fe50855748b34e608b747e761458257f69
Giovanni Terefi <giovanni.terefi@stonybrook.edu>
2023-03-30 22:40:09 -0400

    some more fixes; files should send properly now

---
+++ pubsub/sub.py
@@ -0,0 +1,43 @@
+import zmq
+import time
+import tqdm
+import socket
+import os
+
+BUFFER_SIZE = 4096
+separator = "<SEPARATOR>"
+clientAddress = "tcp://*:5555"
+
+print("Connecting to GEOTIFF over ZMQ...")
+context = zmq.Context()
+subscriber = context.socket(zmq.SUB)
+subscriber.bind(clientAddress)
+subscriber.setsockopt(zmq.SUBSCRIBE, b"")
+
+print(f"Listening over {clientAddress}...")
+fileName = subscriber.recv_string()
+size = subscriber.recv_string()
+size = int(size)
+# fileSocket = context.socket(zmq.SUB)
+print(f"Receiving file {fileName} with size: {size} bytes...")
+
+# fileBuffer = fileSocket.recv(BUFFER_SIZE).decode()
+# fileName, size = fileBuffer.split(separator)
+# fileName = os.path.basename(fileName)
+# size = int(size)
+
+progress = tqdm.tqdm(range(size), f"Receiving {fileName}", unit="B", unit_scale=True, unit_divisor=1024)
+
+with open(fileName, 'wb') as f:
+    while True:
+        bytes_read = subscriber.recv(BUFFER_SIZE)
+        if not bytes_read:
+            break
+        else:
+            f.write(bytes_read)
+            progress.update(len(bytes_read))
+
+# fileSocket.close
+subscriber.close
+    
+time.sleep(5)
